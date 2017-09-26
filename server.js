95aaaa0f3d4b73198e3bf3da97bcb8619a4da046
Anique <aniquetahir@gmail.com>
2017-09-26 10:04:43 -0700

    added utils

--- server.js
+++ server.js
@@ -9,15 +9,25 @@ app.get('/', function (req, res) {
 });
 
 app.get('/datasets.json', function (req, res) {
+    // TODO get attributes from actual data
+    res.setHeader('Content-Type', 'application/json');
     let datasets = [
         {
             id: 301,
             name: 'yellowdata',
-            spatial: [{id:0, }]
-
+            spatial: [{id:1, name: 'Pickup Location'},{id:2, name: 'Dropoff Location'}],
+            nonspatial: [
+                {id:0, name: 'Count'},
+                {id:1, name:'Passenger Count'},
+                {id:2, name: 'Tip Percentage'},
+                {id:3, name: 'Trip Distance'},
+                {id:4, name: 'Tip Amount'},
+                {id:5, name: 'Total Amount'},
+                {id:6, name: 'Tip Percentage'},
+            ]
         }
     ];
-    res.send('Hello World!')
+    res.send(JSON.stringify(datasets));
 });
 
 app.listen(8080, function () {
